e71d6fe2bfaa53a53d35e6cba31986f02d460390
Саша Ларцев <sasha.lartsev@gmail.com>
2026-02-10 01:13:07 +0400

    book recognition serivice

--- BookShop/BookShop.AppHost/AppHost.cs
+++ BookShop/BookShop.AppHost/AppHost.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.GrpcBookService>("grpcbookservice");
 
+builder.AddProject<Projects.GrpcBookRecognitionService>("grpcbookrecognitionservice");
+
 builder.Build().Run();
---
+++ BookShop/GrpcBookRecognitionService/Features/BookRecognition/Services/BookRecognitionGrpcService .cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Core;
+using Book.Recognition.V1;
+
+namespace GrpcBookRecognitionService.Features.BookRecognition.Services
+{
+    public class BookRecognitionGrpcService : BookRecognitionService.BookRecognitionServiceBase
+    {
+        public override async Task<RecognizeBookResponse> RecognizeBook(
+            RecognizeBookRequest request,
+            ServerCallContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override async Task<RecognizeBooksResponse> RecognizeBooks(
+            RecognizeBooksRequest request,
+            ServerCallContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
