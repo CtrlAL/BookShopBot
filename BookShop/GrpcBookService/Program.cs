13a976348eff83035482692608620dcf60c143f5
Larcev.Aleksandr <sasha.lartsev@gmail.com>
2026-02-10 16:08:07 +0400

    fix build

--- BookShop/GrpcBookService/Program.cs
+++ BookShop/GrpcBookService/Program.cs
@@ -18,8 +18,7 @@ var app = builder.Build();
 app.MapDefaultEndpoints();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-//app.MapGrpcService<BookGrpcService>();
+app.MapGrpcService<BookGrpcService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
