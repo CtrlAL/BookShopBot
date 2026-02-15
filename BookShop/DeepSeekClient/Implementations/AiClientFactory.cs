94203877a32bda6684b7e37bf2d71f83570e5cb1
Саша Ларцев <sasha.lartsev@gmail.com>
2026-02-15 19:54:07 +0400

    Fix Bookshop

---
+++ BookShop/DeepSeekClient/Implementations/AiClientFactory.cs
@@ -0,0 +1,32 @@
+﻿using DeepSeek.Configs;
+using Microsoft.Extensions.Options;
+using OpenAI;
+using System.ClientModel;
+
+namespace DeepSeek.Implementations
+{
+    public class AiClientFactory
+    {
+        private readonly DeepSeekVisionService _visionService;
+        private readonly DeepSeekConfig _config;
+
+        public AiClientFactory(
+            IOptions<DeepSeekConfig> options,
+            DeepSeekVisionService visionService)
+        {
+            _config = options.Value;
+            _visionService = visionService;
+        }
+
+        public OpenAIClient Create()
+        {
+            var clientOptions = new OpenAIClientOptions()
+            {
+                Endpoint = new Uri(_config.BaseUrl),
+            };
+            var credential = new ApiKeyCredential(_config.ApiKey);
+
+            return new OpenAIClient(credential, clientOptions);
+        }
+    }
+}
